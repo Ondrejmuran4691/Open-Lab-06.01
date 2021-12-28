74b221ee713d0eac0cfecb4d15f4cd54db7ec30d
Ondrejmuran4691 <58992354+Ondrejmuran4691@users.noreply.github.com>
2021-12-28 21:02:18 +0100

    write letter multiple times, how many times is set by number after letter

---
+++ Open-Lab-06.01/Open-Lab-06.01/Challenge.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Open_Lab_06._01
+{
+    public class Challenge
+    {
+        public string MysteryFunc(string str)
+        {
+            string final = "";
+            int repeat = 1;
+            string cha = "";
+            
+            int rep = 0;
+            foreach (char c in str)
+            {
+                repeat++;
+                if(repeat % 2 == 0)
+                {
+                    cha = "";
+                    cha = c.ToString();
+                    rep = 0;
+                }
+                else
+                {
+                   
+                    do
+                    {
+                        rep++;
+                        final += cha;
+
+                    } while (rep < int.Parse(c.ToString()));
+
+
+                }
+
+            }
+            return final;
+        }
+    }
+}
